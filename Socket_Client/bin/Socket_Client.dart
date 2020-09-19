6a7085f401ffdd4fdcb4f2f3f8f66672b128db09
Orlando Eduardo Pereira <orlando.eduardo101@gmail.com>
2020-09-19 18:17:51 -0300

    UDP

--- Socket_Client/bin/Socket_Client.dart
+++ Socket_Client/bin/Socket_Client.dart
@@ -1,8 +1,20 @@
 
+import 'dart:convert';
 import 'dart:io';
 
-void main(List<String> arguments) async {
-  bool loop = true;
+void main(List<String> arguments)  {
+  var data = "******* Send Informations UDP Protocol *********";
+  int port = 3000;
+  List<int> dataToSend = utf8.encode(data);
+
+  RawDatagramSocket.bind(InternetAddress.loopbackIPv4, 3000).then((value) {
+
+    value.send(dataToSend, InternetAddress.loopbackIPv4, port);
+
+  });
+
+
+  /*bool loop = true;
 
   var socket = await Socket.connect('127.0.0.1', 3000);
   
@@ -11,6 +23,6 @@ void main(List<String> arguments) async {
     String text = stdin.readLineSync();
     socket.write(text);
   }
-  await socket.close();
+  await socket.close();*/
   exit(0);
 }
--- socket_server/bin/socket_server.dart
+++ socket_server/bin/socket_server.dart
@@ -2,8 +2,25 @@ import 'dart:io';
 import 'dart:convert';
 
 
-void main(List<String> arguments) async {
-  var serverSocket = await ServerSocket.bind('127.0.0.1', 3000);
+void main(List<String> arguments) {
+  var data = "******* Send Informations UDP Protocol *********";
+  List<int> dataToSend = utf8.encode(data);
+  int port = 3000;
+
+  //server
+  RawDatagramSocket.bind(InternetAddress.loopbackIPv4, port).then((value){
+    value.listen((event) {
+      if(event == RawSocketEvent.read){
+        print('Server Listening...');
+        Datagram dg = value.receive();
+        print(utf8.decode(dg.data));
+      }
+    });
+    value.send(dataToSend, InternetAddress.loopbackIPv4, port);
+  });
+
+
+  /*var serverSocket = await ServerSocket.bind('127.0.0.1', 3000);
   print('Listening...');
 
   await for(var socket in serverSocket){
@@ -11,5 +28,5 @@ void main(List<String> arguments) async {
      // print('${socket.remoteAddress} : ${socket.remotePort} = ${utf8.decode(event)}');
     print('Client say: ${utf8.decode(event)}');
     });
-  }
+  }*/
 }
