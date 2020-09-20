c41e9baaf6307be92c9aff3d0f720980a17a9f86
Orlando Eduardo Pereira <orlando.eduardo101@gmail.com>
2020-09-20 20:12:30 -0300

    Groupament

--- testes/test/testes_test.dart
+++ testes/test/testes_test.dart
@@ -6,13 +6,25 @@ void main() {
     expect(calculate(), 42);
   });*/
   
-  test('String.split, divider string.', (){
-    String text = "test,test1,test2";
-    expect(text.split(','), equals(['test', 'test1', 'test2']));
+  group('String',(){
+    test('String.split, divider string.', (){
+      String text = "test,test1,test2";
+      expect(text.split(','), equals(['test', 'test1', 'test2']));
+    });
+
+    test('String.trim(), remove space on string', (){
+      String text = ' house ';
+      expect(text.trim(), equals('house'));
+    });
   });
 
-  test('String.trim(), remove space on string', (){
-    String text = ' house ';
-    expect(text.trim(), equals('house'));
+  group('Int', (){
+    test('.reminder, return rest of Division', (){
+      expect(17.remainder(3), equals(2));
+    });
+    
+    test('.toRadixString() return hex string', (){
+      expect(11.toRadixString(16), equals('b'));
+    });
   });
 }
