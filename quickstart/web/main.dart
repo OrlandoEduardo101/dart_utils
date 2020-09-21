bc6b73018a6be85d255a9fc916051bba9969e123
Orlando Eduardo Pereira <orlando.eduardo101@gmail.com>
2020-09-20 22:58:39 -0300

    via cep api

--- quickstart/web/main.dart
+++ quickstart/web/main.dart
@@ -1,9 +1,23 @@
 import 'dart:html';
 
+import 'package:dio/dio.dart';
+
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  /* querySelector('#output').text = 'Your Dart app is running.';
   querySelector('#output').onClick.listen((event) {
     querySelector('#output').text = 'fala zeze.';
     print('click');
+  });*/
+
+  querySelector('#search').onClick.listen((event) async {
+    var cep = (querySelector('#cep') as InputElement).value;
+    var url = 'https://viacep.com.br/ws/$cep/json/';
+
+    var response = await Dio().get(url);
+    print(response.data);
+    (querySelector('#state') as InputElement).value = response.data['uf'];
+    (querySelector('#city') as InputElement).value = response.data['localidade'];
+    (querySelector('#neighborhood') as InputElement).value = response.data['bairro'];
+    (querySelector('#street') as InputElement).value = response.data['logradouro'];
   });
 }
